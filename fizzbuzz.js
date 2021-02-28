96446b65ad8886d62e1f83dccb05f2bc8098d2ef
unknown <ste_carey@live.co.uk>
2021-02-28 15:49:40 +0000

    uploading file

---
+++ fizzbuzz.js
@@ -0,0 +1,26 @@
+for (let number = 1; number <=100; number++){
+    if(number % 3 === 0 &&  number % 5 === 0){
+        console.log('FizzBuzz');
+    }
+    else if (number % 3 === 0){
+        console.log('Fizz')
+    }
+    else if (number % 5 === 0){
+        console.log('Buzz')
+    }
+    else{
+        console.log(number)
+    }
+}
+
+// improved
+
+for (let number = 1; number <=100; number++){
+
+    let output = '';
+    if(number % 3 === 0) {output += 'Fizz';}
+    if(number % 5 === 0) {output += 'Buzz';}
+
+    if(output ==='') {output = number;}
+    console.log(output)
+}
